603c0b54288ba23fc595148219eb0b059ec9b076
ishandeveloper <ishandeveloper@outlook.com>
2020-05-21 16:31:07 -0400

    Null Cases

--- mac_changer.py
+++ mac_changer.py
@@ -9,7 +9,12 @@ def get_arguments():
     parser.add_option("-i", "--interface", dest="interface", help="Interface whose MAC Address you want to change")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
     (options, arguments) = parser.parse_args()
-    mac_changer(options.interface, options.new_mac)
+    if not options.interface:
+        print('[!] Please Select An Interface Using -i \n\tFor Example : -i eth0')
+    if not options.new_mac:
+        print('[!] Please Type New Mac Address Using -m \n\tFor Example : -m 00:11:22:33:44:55')
+    else:
+        mac_changer(options.interface, options.new_mac)
 
 
 def mac_changer(interface, new_mac):
