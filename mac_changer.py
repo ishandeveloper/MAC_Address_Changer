790363190bd73db26f485ae5053ad92762af545b
ishandeveloper <ishandeveloper@outlook.com>
2020-05-21 16:03:10 -0400

    Parse Arguments

--- mac_changer.py
+++ mac_changer.py
@@ -1,6 +1,12 @@
 #!usr/bin/env python3
 
 import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface whose MAC Address you want to change")
+parser.parse_args()
 
 interface = input("INTERFACE > ")
 new_mac = input(" NEW MAC ADDRESS > ")
