fdf488f6ac0bd4aae095dd667d3c0bb4a9bb774d
ishandeveloper <ishandeveloper@outlook.com>
2020-05-21 16:24:42 -0400

    Refactor

--- mac_changer.py
+++ mac_changer.py
@@ -4,6 +4,14 @@ import subprocess
 import optparse
 
 
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface whose MAC Address you want to change")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
+    (options, arguments) = parser.parse_args()
+    mac_changer(options.interface, options.new_mac)
+
+
 def mac_changer(interface, new_mac):
     print("[+] Changing MAC Address For " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
@@ -12,11 +20,5 @@ def mac_changer(interface, new_mac):
     print('[✓] MAC ADDRESS SUCCESSFULLY CHANGED TO ' + new_mac + ' FOR ' + interface)
 
 
-parser = optparse.OptionParser()
-
-parser.add_option("-i", "--interface", dest="interface", help="Interface whose MAC Address you want to change")
-parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
-(options, arguments) = parser.parse_args()
-
-mac_changer(options.interface, options.new_mac)
+get_arguments()
 
