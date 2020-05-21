bf87eacae990638f3bae9a760fba259476df6ec6
ishandeveloper <ishandeveloper@outlook.com>
2020-05-21 16:18:35 -0400

    Options

--- mac_changer.py
+++ mac_changer.py
@@ -6,10 +6,11 @@ import optparse
 parser = optparse.OptionParser()
 
 parser.add_option("-i", "--interface", dest="interface", help="Interface whose MAC Address you want to change")
-parser.parse_args()
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
+(options, arguments) = parser.parse_args()
 
-interface = input("INTERFACE > ")
-new_mac = input(" NEW MAC ADDRESS > ")
+interface = options.interface
+new_mac = options.new_mac
 
 print("[+] Changing MAC Address For "+interface+" to "+new_mac)
 
@@ -17,3 +18,4 @@ subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
 subprocess.call(["ifconfig", interface, "up"])
 
+print('[✓] MAC ADDRESS SUCCESSFULLY CHANGED TO '+new_mac+' FOR '+interface)
